5c26dc04881eab8ecfbca0fe31af1a2fdd018b6a
Adam Golota <golota.adam@gmail.com>
2018-10-06 16:15:39 +0300

    Add start component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Start from './start';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Start />
       </div>
     );
   }
---
+++ client/src/components/start.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Input, FormGroup, Form } from 'reactstrap';
+
+const maxLen = 20;
+const messages = {
+    wrongInput: "Sorry, only a-z, A-Z and _ characters are allowed",
+    tooLong: `Sorry, username must be no longer than ${maxLen} characters.`
+};
+
+
+class Start extends Component {
+    state = {
+        username: "",
+        message: ""
+    }
+    handleChange = (e) => {
+        let username = e.target.value;
+        if (!isLiteral(username) && username) {
+            this.msg = messages.wrongInput;
+            return;
+        }
+        if (username.length > maxLen) {
+            this.msg = messages.tooLong;
+            return;
+        }
+        this.username = username;
+    }
+    set msg(str) {
+        this.setState({ message: str });
+    }
+    set username(username) {
+        this.setState({ username: username });
+    }
+    get isSubmitDisabled() {
+        return !this.state.username;
+    }
+
+
+    render() {
+        return (
+            <Form>
+                <FormGroup>
+                    <p>Username</p>
+                    <Input
+                        name="username"
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <Input
+                        type="submit"
+                        value="Start session!"
+                        disabled={this.isSubmitDisabled}
+                    />
+                </FormGroup>
+            </Form>
+        );
+    }
+}
+
+function isLiteral(str) {
+    return !str.match(/[^a-z_]/i);
+}
+
+export default Start;
